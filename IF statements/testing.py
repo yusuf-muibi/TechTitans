ddc73c7870099ee93d0b729d51652ddd16842e61
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2024-01-22 16:58:26 +0100

    First push

---
+++ IF statements/testing.py
@@ -0,0 +1,15 @@
+username_str = input("Good day sir/ma, Please state your name: ")
+
+print(f"Hey {username_str}! Let's check if you're eligible to vote.")
+
+user_age = input("Please enter your age: ")
+
+age = int(user_age)
+
+votingAge_range = 18
+
+
+if age >= votingAge_range:
+    print(f"Hi {username_str}, You are eligible to vote!")
+else:
+    print(f"Hi {username_str} Sorry, you are not eligible to vote yet.")
