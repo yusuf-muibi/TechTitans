f87be792a56a2da45a5832a7f6aae548e81b50fb
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2024-02-21 23:28:31 +0100

    shuffle

---
+++ IF statements/random_module/random_task.py
@@ -0,0 +1,42 @@
+# Generate two random numbers between 1 and 100. 
+# Ask the user to calculate their sum.
+import random
+
+random_a = random.randint(1, 100)
+random_b = random.randint(1, 100)
+print(random_a)
+print(random_b)
+
+random_sum = (random_a + random_b)
+print(random_sum)
+
+# Randomly shuffle the letters of a word 
+# and ask the user to unscramble it to form the original word.
+
+# def shuffle(word):
+#     shuffle = input("Your word: ")
+#     return shuffle
+
+# generate random word
+# shuffle the letter
+# print shuffle letters
+# ask user to guess the word/ unscrable
+    # loop to keep looping untill its true
+    # Give user three trial
+
+import random
+random_word = input("Enter you word: ")
+shuffled_word = random.sample(random_word, len(original_word))
+
+
+
+
+
+
+
+
+
+
+
+
+
