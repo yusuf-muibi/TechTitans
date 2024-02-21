35275abed2890653290729c25b82c1ea94731a79
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2024-02-21 22:17:07 +0100

    random num

---
+++ IF statements/rand.py
@@ -0,0 +1,26 @@
+import random
+
+random_a = random.randint(1, 100)
+random_b = random.randint(1, 100)
+
+print(random_a)
+print(random_b)
+
+random_sum = (random_a + random_b)
+print(random_sum)
+
+
+# # Randomly shuffle the letters of a word 
+# # and ask the user to unscramble it to form the original word.
+
+# #generate random word
+# #shuffle the letter
+# #print shuffle letters
+# #ask the user to guess the word
+# # loop to keep looping until its true
+# #give user three trials
+
+# import random
+# random_word = input("Enter a word: ")
+
+
