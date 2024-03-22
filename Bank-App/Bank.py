b5244876bc42231d048a08b321fc664a361c9abc
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2024-03-22 22:18:04 +0100

    Ok

--- Bank-App/Bank.py
+++ Bank-App/Bank.py
@@ -74,6 +74,8 @@ def signup():
         cus_info["password"] = cus_pword
 
         """To first of all load the existing database"""
+        with open(techtitans_database, "r") as td:
+                cus_data = json.load(td)
 
         """To add the user information to the database"""
         cus_data[cus_info["Username"]] = cus_info
@@ -83,11 +85,9 @@ def signup():
             json.dump(cus_data, td, indent=2)
 
         print(f"Successfully signed up {cus_info['Username']}")
-        if path.exists("techtitans_database") and path.getsize > 0:
-            with open(techtitans_database, "r") as td:
-                cus_data = json.load(td)
-                for k,v in cus_data.item():
-                    cus_data[k] = v
+        # if path.exists("techtitans_database") and path.getsize > 0:
+        #         for k,v in cus_data.item():
+        #             cus_data[k] = v
 
         signin_option = input("Do you want to sign in now ? (yes/no): ")
 
