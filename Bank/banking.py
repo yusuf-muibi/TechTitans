e067ae7c30c6875028c61799c98d664495daaa10
Muslimbello <yuusufyusasif@gmail.com>
2024-03-23 22:19:48 +0100

    fixed deposit

--- Bank/banking.py
+++ Bank/banking.py
@@ -1,13 +1,25 @@
-import home
+class BankAccount:
+    def __init__(self, name, balance=0):
+        self.account_holder = name
+        self.bal = balance
+
+    def deposit(self, amount):
+        self.bal = self.bal + amount
+        print(f"Deposited {amount} to your account")
+
+    def withdrawal(self, amount):
+        if amount > self.bal:
+            print(f"Insufficient fund, your available balance is {self.bal}")
+
+        else:
+            self.bal = self.bal - amount
+
+    def __str__(self):
+        return f"Account Holder Name: {self.account_holder} \nBalance: {self.bal}"
 
 
-def check_balance():
-    balance = 0.00
-    if home.signin == True:
-        print(balance)
-
-
-check_balance()
-
-# def deposit():
-#     amount = int(input("Input the amount you want to deposit"))
+obj = BankAccount("Tobi", 2000)
+print(obj)
+obj.deposit(200)
+obj.withdrawal(500)
+print(obj)
